32f0ad085299553d5c04835b6ead79af33bb0cd9
Vinh Hoang <64019402+vinhhn3@users.noreply.github.com>
2022-07-01 13:19:46 +0700

    Display alert

--- www/js/index.js
+++ www/js/index.js
@@ -26,4 +26,8 @@ function onDeviceReady() {
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
+
+    alert("Platform " + cordova.platformId +
+        "\n" + "Version: " + cordova.version
+    );
 }
